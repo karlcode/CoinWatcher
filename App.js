a87d378e35c9e141d9ca758d90ee3cd8e69f1b21
karlcode <karl.l1@hotmail.com>
2018-01-19 00:43:09 +1100

    Playing around with API

--- App.js
+++ App.js
@@ -1,21 +1,48 @@
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button, Alert } from 'react-native';
-
+import { StyleSheet, Text, View, Image, Alert } from 'react-native';
+import { Button, Header,List, ListItem } from 'react-native-elements';
+const list = [
+  {
+    title: 'Appointments',
+    icon: 'av-timer'
+  },
+  {
+    title: 'Trips',
+    icon: 'flight-takeoff',
+    subtitle: 'Vice Chairman'
+  },
+  {
+    title: 'Trips',
+    icon: 'rowing',
+    subtitle: 'Vice Chairman'
+  },
+]
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <List>
+          {
+            list.map((item, i) => (
+              <ListItem
+                key={i}
+                title={item.title}
+                leftIcon={{name: item.icon}}
+              />
+            ))
+          }
+        </List>
         <Image
           source={{uri: 'https://i.chzbgr.com/full/7345954048/h7E2C65F9/'}}
           style={{width: 320, height:180}}
         />
-        <Text>Open up App.js to start working on your map!</Text>
-        <Text>Changes you make will automatically reload.</Text>
         <Button
-          onPress={() => {
-          Alert.alert('You tapped the button!');
-          }}
-          title="Press Me"
+          onPress={()=>{Alert.alert("asd")}}
+          raised
+          icon={{name: 'home', size: 20}}
+          buttonStyle={{backgroundColor: 'pink', borderRadius: 10}}
+          textStyle={{textAlign: 'center'}}
+          title={`Welcome to\nReact Native Elements`}
         />
         <Text>Shake your phone to open the developer menu.</Text>
       </View>
@@ -29,5 +56,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
   },
 });
